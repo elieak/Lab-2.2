253d7f817985d02230130f1daa8650cd6d4cef59
Ytek <Ytek@YTEK-PC>
2016-06-17 11:35:43 +0300

    2

---
+++ Lab2.2/Lab2.2/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab2._2
+{
+    internal class Calculator
+    {
+        private static void Main()
+        {
+            var userInput = string.Empty;
+            Console.Write(("heheha"));
+        }
+    }
+}
